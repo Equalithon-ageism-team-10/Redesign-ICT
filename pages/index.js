a3e2a7913762d88b02971756e3cf3bacb3420844
Jean Carlos Zhong <64503277+jeancarlosZ@users.noreply.github.com>
2022-01-21 10:51:16 -0500

    feat: some of the chatbot's possible choices and responses

--- pages/index.js
+++ pages/index.js
@@ -23,26 +23,110 @@ export default function Home() {
               steps={[
                 {
                   id: "1",
-                  message: "What number I am thinking?",
+                  message: "Hi There! Would you like to talk about your career?",
                   trigger: "2",
                 },
                 {
                   id: "2",
                   options: [
-                    { value: 1, label: "Number 1", trigger: "4" },
-                    { value: 2, label: "Number 2", trigger: "3" },
-                    { value: 3, label: "Number 3", trigger: "3" },
+                    { value: 1, label: "Let me tell you!", trigger: "4" },
+                    { value: 2, label: "Not today.", trigger: "3" },
                   ],
                 },
                 {
                   id: "3",
-                  message: "Wrong answer, try again.",
-                  trigger: "2",
+                  message: "No worries! Chat soon!",
+                  end: true,
                 },
                 {
                   id: "4",
-                  message: "Awesome! You are a telepath!",
-                  end: true,
+                  message: "Awesome! How do you feel about work today?",
+                  trigger: "5",
+                },
+                {
+                  id: "5",
+                  options: [
+                    { value: 1, label: "Great!", trigger: "" },
+                    { value: 2, label: "OK.", trigger: "" },
+                    { value: 3, label: "Not Good", trigger: "6" },
+                  ],
+                },
+                {
+                  id: "6",
+                  message: "Tomorrow will be better! How is your work-life balance?",
+                  trigger: "7",
+                },
+                {
+                  id: "7",
+                  options: [
+                    { value: 1, label: "Perfect!", trigger: "" },
+                    { value: 2, label: "Its OK", trigger: "" },
+                    { value: 3, label: "Help Me!", trigger: "8" },
+                  ],
+                },
+                {
+                  id: "8",
+                  message: "Sorry to hear that. Do you like what you do?",
+                  trigger: "9",
+                },
+                {
+                  id: "9",
+                  options: [
+                    { value: 1, label: "100%", trigger: "" },
+                    { value: 2, label: "Debatable", trigger: "10" },
+                    { value: 3, label: "Not really", trigger: "" },
+                  ],
+                },
+                {
+                  id: "10",
+                  message: "Understandable. Is there any growth in this career for you?",
+                  trigger: "11",
+                },
+                {
+                  id: "11",
+                  options: [
+                    { value: 1, label: "I'd like to", trigger: "" },
+                    { value: 2, label: "Not Sure", trigger: "12" },
+                    { value: 3, label: "Not really", trigger: "" },
+                  ],
+                },
+                {
+                  id: "12",
+                  message: "It sounds like a career change might be good. What do you think?",
+                  trigger: "13",
+                },
+                {
+                  id: "13",
+                  options: [
+                    { value: 1, label: "Yes!!", trigger: "" },
+                    { value: 2, label: "Not right now", trigger: "" },
+                    { value: 3, label: "But how?", trigger: "14" },
+                    { value: 4, label: "Isn't it too late?", trigger: "" },
+                  ],
+                },
+                {
+                  id: "14",
+                  message:
+                    "At ICT we have a 12-week career transition program. We go step-by-step with you through it",
+                  trigger: "15",
+                },
+                {
+                  id: "15",
+                  message: "We also have 1:1 coaching so you're never alone.",
+                  trigger: "16",
+                },
+                {
+                  id: "16",
+                  message: "Finally, check out our career library for free resources!",
+                  trigger: "17",
+                },
+                {
+                  id: "17",
+                  options: [
+                    { value: 1, label: "Program", trigger: "" },
+                    { value: 2, label: "1:1 Coach", trigger: "" },
+                    { value: 3, label: "Library", trigger: "" },
+                  ],
                 },
               ]}
             />
