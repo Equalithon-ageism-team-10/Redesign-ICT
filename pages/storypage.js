317c7a006b9020fff42bb649b70df3bbf8df5d0c
Jean Carlos Zhong <64503277+jeancarlosZ@users.noreply.github.com>
2022-01-22 22:51:00 -0500

    style: add styling to StoryPage component

--- pages/storypage.js
+++ pages/storypage.js
@@ -1,5 +1,18 @@
+import styles from "../styles/StoryPage.module.css";
+import Head from "next/head";
+
 const StoryPage = () => {
-  return <div>Storybook</div>;
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Redesign-ICT-StoryPage</title>
+        <meta name="Redesign-ICT-StoryPage" content="Marketing idea for ICT" />
+        <link rel="icon" href="/ict/ictLogo.png" />
+      </Head>
+
+      <main className={styles.main}>Storypage</main>
+    </div>
+  );
 };
 
 export default StoryPage;
