ff349c7690b0844998e4d46f27d6fe5eda74e261
Jean Carlos Zhong <64503277+jeancarlosZ@users.noreply.github.com>
2022-01-24 22:35:17 -0500

    feat: added description for each phase of the metamorphosis

--- pages/storypage.js
+++ pages/storypage.js
@@ -22,11 +22,29 @@ const StoryPage = () => {
             alt="caterpillar image"
           />
         </div>
+        <div className={styles.card}>
+          <h2>Learning As Much As Possible</h2>
+          <p>
+            Learning strategies, assessments, to have a new purpose, productivity habits, and to
+            craft a personalized routine{" "}
+          </p>
+        </div>
         <div className={cls("glass", styles.card)}>
           <Image src="/butterfly/pupa.png" width="128" height="256" alt="pupa image" />
         </div>
+        <div className={styles.card}>
+          <h2>Incorporating Into Routine What Was Learned</h2>
+          <p>Networking, branding, sprinting, evaluating, and modifying routine as necessary</p>
+        </div>
         <div className={cls("glass", styles.card)}>
           <Image src="/butterfly/butterfly.png" width="256" height="256" alt="butterfly image" />
+        </div>
+        <div className={styles.card}>
+          <h2>Spread Out Wings and Soar High As New Best Self</h2>
+          <p>
+            Interviewing, accepting offers, onboarding, networking, and bringing others to go
+            through the transformation
+          </p>
         </div>
       </main>
     </div>
